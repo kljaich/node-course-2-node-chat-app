604f5c2f1ecc2ee6c0a6fbaed8c6695777531b31
Joseph Kljaich <kljaich@gmail.com>
2018-07-11 14:45:18 -0500

    Add connection and disconnect listen events

--- server/server.js
+++ server/server.js
@@ -1,21 +1,35 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname, '../public');
-console.log(publicPath);
 
 // Heroku sets process.env.PORT, otherwise use port 3000 locally
 const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 // Configure express app
 app.use(express.static(publicPath));
+
+// Listen for new connections and print out a message when
+// once is detected.  Note, connection is built-in.
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  // Listen for disconnects
+  socket.on('disconnect', () => {
+    console.log('Disconnect from client');
+  })
+});
 
 // app.get('/', (req, res) => {
 //   response.render('index.html');
 // });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Started on port ${port}`);
 })
