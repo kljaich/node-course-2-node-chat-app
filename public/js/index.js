9b666a732ee0cbbf041f95270af013fe46d2ef46
Joseph Kljaich <kljaich@gmail.com>
2018-07-12 10:53:16 -0500

    Add newMsg and createMsg events

---
+++ public/js/index.js
@@ -0,0 +1,35 @@
+var socket = io();
+
+// Listen for builtin connect events from the server
+socket.on('connect', function () {
+  console.log('Connected to server');
+
+  // Emit a createEmail event to the server after a
+  // socket.io connection was established
+  // socket.emit('createEmail', {
+  //   to: "yourMama@fuckyou",
+  //   text: "Take out the garbage you lazy fucker"
+  // });
+
+  // Emit a createMessage event to the server after a
+  // socket.io connection was established
+  socket.emit('createMessage', {
+    from: "me@fuck.you",
+    text: "This is a test message for the Chat Room"
+  })
+});
+
+// Listen for builtin disconnect events from the server
+socket.on('disconnect', function () {
+  console.log('Disconnected to server');
+});
+
+// Listen for a custom newEmail event sent from the server
+// socket.on('newEmail', function (email) {
+//   console.log('New email arrived', email);
+// });
+
+// Listen for a custom newMessage event sent from the server
+socket.on('newMessage', function (message) {
+  console.log('New chat room message arrived', message);
+});
--- server/server.js
+++ server/server.js
@@ -15,12 +15,38 @@ var io = socketIO(server);
 // Configure express app
 app.use(express.static(publicPath));
 
-// Listen for new connections and print out a message when
-// once is detected.  Note, connection is built-in.
+// Listen for new connection events from user clients
+// and print out a message when one is received.
+// Note, a connection event is built-in.
 io.on('connection', (socket) => {
-  console.log('New user connected');
+  console.log('New user client connected');
 
-  // Listen for disconnects
+  // Create and send newEmail event to a user client
+  // socket.emit('newEmail');
+  // socket.emit('newEmail', {
+  //   from: 'mike@example.com',
+  //   text: 'Hey. What is going on',
+  //   createdAt: 123
+  // });
+
+  // Create listen event for createEmail from a user client
+  // socket.on('createEmail', (newEmail) => {
+  //   console.log('createEmail', newEmail);
+  // });
+
+  // Create and send newMessage event to a user client
+  socket.emit('newMessage', {
+    from: 'admin@example.com',
+    text: 'You are getting removed from the chat room',
+    createdAt: 123
+  });
+
+  // Create listen event for createMessage from a user client
+  socket.on('createMessage', (newMessage) => {
+    console.log('createMessage', newMessage);
+  });
+
+  // Listen for builtin disconnect events from user clients
   socket.on('disconnect', () => {
     console.log('Disconnect from client');
   })
