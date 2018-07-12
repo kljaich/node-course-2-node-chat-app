57a7c4d06cf31542665fe6d7ba3ad692021d2ef5
Joseph Kljaich <kljaich@gmail.com>
2018-07-12 11:46:14 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -41,17 +41,39 @@ io.on('connection', (socket) => {
   //   createdAt: 123
   // });
 
+  // Send a broadcast message saying a new user client joined
+  socket.broadcast.emit('newMessage', {
+    from: 'admin',
+    text: 'New user client has joined the Chat App',
+    createdAt: new Date().getTime()
+  });
+
+  socket.emit('newMessage', {
+    from: 'admin',
+    text: 'Welcome to the Chat App',
+    createdAt: new Date().getTime()
+  });
+
   // Create custom listen event for createMessage from a user client
   socket.on('createMessage', (newMessage) => {
     console.log('createMessage', newMessage);
 
-    // Send broadcast message to all user clients when a message is
+    // Send broadcast message to all user clients icluding the user
+    // client that sent the message above, when a message is
     // received from a user client
     io.emit('newMessage', {
       from: newMessage.from,
       text: newMessage.text,
       createdAt: new Date().getTime()
     });
+
+    // Send broadcast message to all user clients except the one
+    // that sent the message above.
+    // socket.broadcast.emit('newMessage', {
+    //     from: newMessage.from,
+    //     text: newMessage.text,
+    //     createdAt: new Date().getTime()
+    // });
   });
 
   // Listen for builtin disconnect events from user clients
